176b5efa4572912a0f2606d5bdc14cd15f6c01c9
engyaya <52547021+engyaya@users.noreply.github.com>
2019-07-29 22:10:54 +0300

    Update index.php
    حل tasks 3,4,5

--- index.php
+++ index.php
@@ -30,3 +30,50 @@ for($j = 5 ; $j > -5; $j--){
 }
 
 d) 5 4 3 2 1 0 -1 -2 -3 -4
+<?php
+// Task (3)
+function getMax($array)
+{
+   $n = count($array);
+   $max = $array[0];
+   for ($i = 1; $i < $n; $i++)
+       if ($max < $array[$i])
+           $max = $array[$i];
+    return $max;
+}
+echo "<br>";
+
+function getMin($array)
+{
+   $n = count($array);
+   $min = $array[0];
+   for ($i = 1; $i < $n; $i++)
+       if ($min > $array[$i])
+           $min = $array[$i];
+    return $min;
+
+}
+
+$array = array(12,26,34,28,19,37,9);
+echo(getMax($array));
+echo "<br>";
+echo(getMin($array));
+echo "<br>";
+echo (getMax($array)) + (getMin($array));
+echo "<br>","<br>","<br>";
+
+// Task(4)
+$str = array("php","java","html","CSS","python","json");
+$arr = array (0,1,2,3,4,5);
+echo $str[3];
+echo "<br>","<br>","<br>";
+
+// Task(5)
+$str = "compare between php and another lang java";
+echo $str."<br>";
+
+$str2 = str_replace("php","java",$str);
+echo $str2."<br>";
+
+$str3 = str_replace("java","php",$str);
+echo $str3;
